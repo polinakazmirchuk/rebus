b72c9d0dc6fff43faf65fe2d37ecccd0a3ae5d5c
Polina Kazmirchuk <kazmircukpolina1mp@gmail.com>
2023-02-19 11:45:02 +0200

    added files

---
+++ script1.js
@@ -0,0 +1,19 @@
+
+function answer() {
+    //let text = "correct!".innerText;
+
+    if (input.value.toLowerCase() == "флексбокс") {
+        main.style.backgroundColor = "#388852";
+        header.style.backgroundColor = "#388852";
+        button.style.backgroundColor = "#388852";
+        //input.value.inerrText = "text";
+        //input.value.innerText = "Correct"
+
+    }
+    else {
+        main.style.backgroundColor = "#A72828";
+        header.style.backgroundColor = "#A72828";
+        button.style.backgroundColor = "#A72828";
+    }
+}
+
---
+++ script3.js
@@ -0,0 +1,16 @@
+
+function answer() {
+
+    if (input.value.toLowerCase() == "контейнер") {
+        main.style.backgroundColor = "#388852";
+        header.style.backgroundColor = "#388852";
+        button.style.backgroundColor = "#388852";
+
+    }
+    else {
+        main.style.backgroundColor = "#A72828";
+        header.style.backgroundColor = "#A72828";
+        button.style.backgroundColor = "#A72828";
+    }
+}
+
